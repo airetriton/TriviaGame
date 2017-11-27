a18daeb72a35afc8e61b62f1ac3c61dfc9b3d09f
airetriton <wobbe_espinoza@hotmail.com>
2017-11-26 20:41:21 -0700

    redo

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,43 +1,37 @@
-
-
-
-var counter = 120;
-var question;
-var selectedAnswer;
-var wrongLoss;
-var correctWin;
-
 $(document).ready(function() {
-	
+
+	var counter = 45;
+
 	//insert header text into header
 
 	function headerText() {
 		$(".header").append("<h1> Welcome to the World of Dogs Trivia </h1>");
 		$(".header").append("<h3> Answer the Questions Before Time Runs Out </h3>");
-	};
-
-	headerText();
+	}
 	
 	//create the button to start the game and put it into the header
-
 	function headingBtn() {
 		var startGame = $("<div id='begin'></div>"),
-		newButton = $("<button class='btn btn-lg startBtn'>Start</button>");
+		newButton = $("<button class='btn btn-lg startBtn '>Start</button>");
 		$(".header").append(startGame,[newButton]);
-		
+				
 		console.log(headingBtn);
-	};
-
-	headingBtn();
-
+	}
+	
 	// create clock to countdown
 	function timeClock(){
 		var timer = $("<div class='clockdiv' id='clockdiv'></div>");
 		$(".header").append(timer);		
-	};	
+	}	
+	;
+	headerText();
+	headingBtn()
 	timeClock();
 	
+	
 	$(".startBtn").on("click", function(){
+
+		event.preventDefault();
 
 		var countDown = setInterval(function() {
      		counter--;
@@ -49,7 +43,9 @@ $(document).ready(function() {
 	        	clearInterval(countDown);
 	        	document.getElementById("clockdiv").innerHTML = "Sorry, You Ran Out Of Time";
 		       }
+
 	     }, 1000);
+		hide ="#begin";
 	});
 
 	console.log(questionArray);
@@ -57,41 +53,99 @@ $(document).ready(function() {
 	console.log(questionArray[0].choices);
 	console.log(questionArray[0].correctAnswer);
 
+
+var triviaForm = document.getElementById("mainContainer");
+var resultsCon = document.getElementById("results");
+var submitBtn = document.getElementById("submit");
+
+triviaHTML(questionArray, triviaForm, resultsCon, submitBtn);
 	
-	var airedale = "../images/airedalepup.jpg";
 
-	$(".startBtn").on("click", function(){
+	function triviaHTML(questions, triviaForm, resultsCon, submitBtn) {
 
-		function triviaHTML() {
-			for (i=0; i < questionArray.length; i++){
-			question = $("<p class = 'text-center</p>");
-			$(".mainContainer").append(question);
+		function showQuestions(questions, triviaForm){
+			var output = [];
+			var answers;	
+				
+			for(var i = 0; i < questions.length; i++) {
+				
+				answers = [];
 			
+				for(answer in questions[i].answers){
+					answers.push(
+						'<label>'
+						+ '<input type="radio" name="question' + i + '" value="' + answer + '">'  
+						+ questions[i].answers[answer]
+						+ '</label>'
+					);
+				}
 
-			for(j=0; j<(questionArray[i].choices).length; j++){
-			var answerBtn = $("<button>");
-			answerBtn.addClass("text-center btn btn-primary answerBtn");
-			answerBtn.text(questionArray[i].choices[j]);
-			$(".mainContainer").append(answerBtn);
-			};
+			
+				output.push(
+					'<div class="question">' + questions[i].question + '</div>'
+					+ '<div class="answers">' + answers.join('') + '</div>'
+				);
+			}
+			triviaForm.innerHTML = output.join('');				
+		}
 
-			$(".answer").on("click", function(event) {
-				selectedAnswer = $(this).text();
 
-				if(selectedAnswer === correctAnswer){
-					triviaWin();
-					clearInterval(timer);
-				} else {
-					clearInterval(timer);
-					triviaLose();
-				};
-			});
-		};
-		};		
-					
-		triviaHTML();
-	});
+		
+	function showResults(questions, triviaForm, resultsCon){
+
+		var answersCon = triviaForm.querySelectorAll('.answers');
+		var userAnswer = '';
+		var numCorrect = 0;
+
+		for(var i = 0; i < questions.length; i++){
+
+			userAnswer = (answersCon[i].querySelector('input[name=question'+i+']:checked') || {}).value;
+	
+	
+			if(userAnswer === questions[i].correctAnswer){
+				numCorrect++;
+				answersCon[i].style.color = 'green';
+				
+			} else {
+				answersCon[i].style.color = 'red';
+				
+			}
+		}	
+
+		resultsCon.innerHTML = numCorrect + ' out of ' + questions.length;
+
+
+		if(numCorrect===10){
+			$('<img/>', {
+				src: 'assets/images/dog-dancing.jpg',
+				width: '150px',
+			}).prependTo(resultsCon);
+			resultsCon.prepend("Great Job! You Win!!\n");
+		} else {
+			$('<img/>', {
+				src: 'assets/images/sad-dog.jpg',
+				width: '150px',
+			}).prependTo(resultsCon);
+			resultsCon.prepend("You Lose! Do some research and try again later.\n");
+			
+		}
+
+		}
+
+		showQuestions(questions, triviaForm);
+
+		submitBtn.onclick = function(){
+		showResults(questions, triviaForm, resultsCon);
+		clearInterval(timeClock);
+
+		
+		}
+	}
+
+
+
 
 
 
 }); //end of document.ready function
+
--- assets/javascript/query.js
+++ assets/javascript/query.js
@@ -1,69 +1,69 @@
 var questionArray = [
 		{
 			question: "As determined by the American Kennel Club, what is the most popular dog breed?", 
-			choices: ["Golden Retreiver", "Labrador", "German Shepherd", "Border Collie", "Chihuahua"],
+			answers: ["Golden Retreiver", "Labrador", "German Shepherd", "Border Collie", "Chihuahua"],
 			correctAnswer: "Labrador"
 			
 		},	
 		{
 			question: "How many US Presidents have had dogs in the White House?", 
-			choices: ["32", "27", "16", "39", "45"],
+			answers: ["32", "27", "16", "39", "45"],
 			correctAnswer: "27"
 			
 		},	
 		{
 			question: "About how many dogs are in the world?", 
-			choices: ["400 million", "250 million", "600 million", "750 million"],
+			answers: ["400 million", "250 million", "600 million", "750 million"],
 			correctAnswer: "400 million"
 			
 		},	
 		{
 		
 			question: "What animal can dogs be traced back to?", 
-			choices: ["Miacis", "Barbaturex", "Lorises", "Chiropetra", "Bonobo"],
+			answers: ["Miacis", "Barbaturex", "Lorises", "Chiropetra", "Bonobo"],
 			correctAnswer: "Miacis"
 			
 		},	
 		{
 		
 			question: "Human have unique fingerprints, what is a dog's unique identifier?", 
-			choices: ["Paw print", "Bark", "Nose print", "Tongue print", "Bite map"],
+			answers: ["Paw print", "Bark", "Nose print", "Tongue print", "Bite map"],
 			correctAnswer: "Nose print"
 			
 		},	
 		{
 		
 			question: "How many eyelids do dogs have?", 
-			choices: ["Four", "Three", "Two", "One"],
+			answers: ["Four", "Three", "Two", "One"],
 			correctAnswer: "Three"
 			
 		},	
 		{
 		
 			question: "Who owned the most dogs in history?", 
-			choices: ["Kublai Kahn", "King of Norway", "Salvador Dali", "Pope Leo X", "Josephine Bonaparte"],
+			answers: ["Kublai Kahn", "King of Norway", "Salvador Dali", "Pope Leo X", "Josephine Bonaparte"],
 			correctAnswer: "Kublai Kahn"
 			
 		},	
 		{
 		
 			question: "Which breed of dog is completely white at birth?", 
-			choices: ["Airedale Terrier", "Weimaraner", "Siberian Huskies", "Dalmations"],
+			answers: ["Airedale Terrier", "Weimaraner", "Siberian Huskies", "Dalmations"],
 			correctAnswer: "Dalmations"
 			
 		},	
 		{
 		
 			question: "What dog breed is known for its bluish-black tongue?", 
-			choices: ["Akita", "Chow-Chow", "Akbash", "Ibzian Hound"],
+			answers: ["Akita", "Chow-Chow", "Akbash", "Ibzian Hound"],
 			correctAnswer: "Dalmations"
 			
 		},	
 		{
 		
-			question: "Which breed of dog is completely white at birth?", 
-			choices: ["Airedale Terrier", "Weimaraner", "Siberian Huskies", "Dalmations"],
-			correctAnswer: "Dalmations"
+			question: "Which breed of dog changes as it ages both in how it looks and their coat texture?", 
+			answers: ["Airedale Terrier", "Weimaraner", "Siberian Huskies", "Dalmations"],
+			correctAnswer: "Airedale Terrier"
 			
 		},	
 	];
